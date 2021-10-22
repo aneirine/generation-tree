6b235ddac2c87551afa069f4ceaaa4182fe18a24
Anna Shypilova <annashypilova@MacBook-Pro-Anna.local>
2021-10-22 20:56:16 +0300

    Add some logs, remove constructors

--- src/main/java/com/aneirine/generation_tree/jpa/families/FamilyService.java
+++ src/main/java/com/aneirine/generation_tree/jpa/families/FamilyService.java
@@ -2,25 +2,25 @@ package com.aneirine.generation_tree.jpa.families;
 
 import com.aneirine.generation_tree.jpa.families.persistence.Family;
 import com.aneirine.generation_tree.jpa.families.repositories.FamilyRepository;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 import java.util.stream.Collectors;
 
+@Slf4j
 @Service
+@RequiredArgsConstructor
 public class FamilyService {
 
     private final FamilyRepository familyRepository;
 
-    public FamilyService(FamilyRepository familyRepository) {
-        this.familyRepository = familyRepository;
-    }
-
     public void createFamily(String familyName) {
-        Family family = Family.builder()
+        Family savedFamily = familyRepository.save(Family.builder()
                 .name(familyName)
-                .build();
-        familyRepository.save(family);
+                .build());
+        log.info("Saved new family [{}]", savedFamily);
     }
 
     public List<String> getAllFamiliesNames() {
